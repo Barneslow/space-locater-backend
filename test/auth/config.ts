c965e43254e8c27049744832ac427ef1960d35c3
Darrach Barneveld <darrachbarneveld@Darrachs-MacBook-Pro.local>
2023-03-01 11:30:27 +0000

    added IAM roles and authorization wrapper classes, updated testing service

--- test/auth/config.ts
+++ test/auth/config.ts
@@ -2,6 +2,7 @@ export const config = {
   REGION: "us-east-1",
   USER_POOL_ID: "us-east-1_LqCbF1yI5",
   APP_CLIENT_ID: "4614tecubb2k0bfgp9la0iar5n",
+  IDENTITY_POOL_ID: "us-east-1:f96e68a1-b534-4e10-a362-edc4ee51af62",
   TEST_USER_NAME: "barneygumble",
   TEST_USER_PASSWORD: "rQ33saLLq,P",
 };
